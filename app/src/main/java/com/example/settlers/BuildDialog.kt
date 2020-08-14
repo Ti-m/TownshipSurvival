64b9009269aff48f9b78d212d258f6f555b4bead
Ti-m <timbroe1@gmail.com>
2020-08-14 23:24:41 +0200

    Don't crash if requested ressource not available

--- app/src/main/java/com/example/settlers/BuildDialog.kt
+++ app/src/main/java/com/example/settlers/BuildDialog.kt
@@ -20,12 +20,7 @@ class BuildDialog(private val cell: Cell, private val cells: List<Cell>, private
                 BuildingType.Road -> Road()
             }
             cell.building!!.requires.forEach { needed ->
-                //TODO crashes with NoSuchElementException, if no ressources available.
-                // Actually it should not be checked right here, instead the gameloop
-                // needs to calc these each tick.
-                val startCoordinates = cells.first { it.building?.offers?.contains(needed) ?: false}//TODO this is not the closests ...
-                startCoordinates.building!!.markRequested(needed)
-                transportManager.requestTransport(start = startCoordinates.coordinates, end = cell.coordinates, what = needed)
+                transportManager.requestTransport(request = TransportRequest(destination = cell.coordinates, what = needed))
             }
             cell.redraw = true
             //tile.invalidate()
--- app/src/main/java/com/example/settlers/Transport.kt
+++ app/src/main/java/com/example/settlers/Transport.kt
@@ -1,17 +1,15 @@
 package com.example.settlers
 
+import android.util.Log
+
 class TransportManager(private val cells: List<Cell>) {
     //Currently getting delivered
     private val activeTransports: MutableList<Transport> = mutableListOf()
     //Requested ressource is not available, therefore, the transport is pending
-    private val pendingTransports: MutableList<Transport> = mutableListOf()
+    private val pendingTransports: MutableList<TransportRequest> = mutableListOf()
 
-    fun requestTransport(start: Coordinates, end: Coordinates, what: Ressource) {
-        //TODO this should put into pendingTransport, and tick should calc the routes
-        val new = Transport(what)
-        new.route.add(start)
-        calcRoute(start,end, new.route)
-        activeTransports.add(new)
+    fun requestTransport(request: TransportRequest) {
+        pendingTransports.add(request)
     }
 
     private fun calcRoute(start: Coordinates, end: Coordinates, route: MutableList<Coordinates>) {
@@ -36,13 +34,32 @@ class TransportManager(private val cells: List<Cell>) {
     }
 
     fun tick() {
+        pendingTransports.forEach {
+            tickForPending(it)
+        }
+        pendingTransports.removeIf { it.markForRemovel }
+
         activeTransports.removeIf { it.route.count() < 2 }
         activeTransports.forEach {
-            tickFor(it)
+            tickForActive(it)
+        }
+    }
+    private fun tickForPending(request: TransportRequest) {
+        try {
+            val startCoordinates = cells.first { it.building?.offers?.contains(request.what) ?: false }
+            startCoordinates.building!!.markRequested(request.what)
+            val new = Transport(request.what)
+            new.route.add(startCoordinates.coordinates)
+            calcRoute(startCoordinates.coordinates,request.destination, new.route)
+            activeTransports.add(new)
+//            pendingTransports.remove(request)
+            request.markForRemovel = true
+        } catch  (e: NoSuchElementException) {
+            Log.i("TransportManager", "Not enough ressources")
         }
     }
 
-    private fun tickFor(transport: Transport) {
+    private fun tickForActive(transport: Transport) {
         val start = transport.route.get(0)
         val end = transport.route.get(1)
         val cellStart = cells.first { it.coordinates == start }
@@ -89,3 +106,5 @@ class TransportManager(private val cells: List<Cell>) {
 data class Transport(val what: Ressource) {
     val route: MutableList<Coordinates> = mutableListOf()
 }
+
+data class TransportRequest(val destination: Coordinates, val what: Ressource, var markForRemovel: Boolean = false)
